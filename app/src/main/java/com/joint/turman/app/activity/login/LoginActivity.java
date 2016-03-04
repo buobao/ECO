3b62ad739fd2fb6827d6366a66c399e45bfaca06
buobao <1039163450@qq.com>
2016-03-04 17:58:23 +0800

    login button

--- app/src/main/java/com/joint/turman/app/activity/login/LoginActivity.java
+++ app/src/main/java/com/joint/turman/app/activity/login/LoginActivity.java
@@ -12,19 +12,68 @@ import com.joint.turman.app.sys.TurmanApplication;
 /**
  * Created by dqf on 2016/3/2.
  */
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements View.OnClickListener {
     protected static final String TAG = "LoginActivity";
+
+    private Button mLoginButton;
+    private Button mQuickButton;
+    private Button mForgetPswButton;
+    private Button mRegistButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_login);
-        Button login_btn = (Button) findViewById(R.id.act_login_btnLogin);
-        login_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TurmanApplication.openActivity(LoginActivity.this, HomeActivity.class,null,true);
-            }
-        });
+        mLoginButton = (Button) findViewById(R.id.act_login_btnLogin);
+        mQuickButton = (Button) findViewById(R.id.act_login_btn_loginQuick);
+        mRegistButton = (Button) findViewById(R.id.act_login_register);
+        mForgetPswButton = (Button) findViewById(R.id.act_login_forget_password);
+        mLoginButton.setOnClickListener(this);
+        mQuickButton.setOnClickListener(this);
+        mRegistButton.setOnClickListener(this);
+        mForgetPswButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.act_login_btnLogin:
+                TurmanApplication.openActivity(LoginActivity.this, HomeActivity.class, null, true);
+                break;
+            case R.id.act_login_btn_loginQuick:
+                break;
+            case R.id.act_login_register:
+                break;
+            case R.id.act_login_forget_password:
+                break;
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
