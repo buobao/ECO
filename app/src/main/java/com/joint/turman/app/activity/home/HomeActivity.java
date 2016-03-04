bc7ecd25a695ecd460bcba1e905015ecd660b954
buobao <1039163450@qq.com>
2016-03-04 14:26:42 +0800

    menu click listener

--- app/src/main/java/com/joint/turman/app/activity/home/HomeActivity.java
+++ app/src/main/java/com/joint/turman/app/activity/home/HomeActivity.java
@@ -7,14 +7,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.joint.turman.app.R;
 import com.joint.turman.app.ui.drawable.MaterialMenuDrawable;
+import com.joint.turman.customwidget.bottomtab.BottomTab;
+import com.joint.turman.customwidget.bottomtab.BottomTabGroup;
+import com.joint.turman.customwidget.tableview.TableView;
 
 /**
  * Created by dqf on 2016/3/3.
  */
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "HomeActivity";
 
@@ -23,11 +29,13 @@ public class HomeActivity extends AppCompatActivity {
     //侧滑菜单
     private DrawerLayout mDrawerLayout;
     private MaterialMenuDrawable materialMenuDrawable;
-
     private View mLeftMenu;
-
+    //工具栏
     protected Toolbar mActionBar;
-
+    //底部菜单
+    private BottomTabGroup mBottomTabGroup;
+    BottomTab mTab1,mTab2,mTab3;
+    private LinearLayout mContent;
 
     private boolean isMenuOpened;
 
@@ -36,8 +44,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_home);
-
-        mLeftMenu = findViewById(R.id.act_home_leftMenu);
 
         //设置顶部工具栏
         mActionBar = (Toolbar) findViewById(R.id.act_home_actionbar);
@@ -83,6 +89,48 @@ public class HomeActivity extends AppCompatActivity {
                 }
             }
         });
+
+        //注册菜单事件
+        mLeftMenu = findViewById(R.id.act_home_leftMenu);
+        ViewGroup viewGroup = (ViewGroup) mLeftMenu.findViewById(R.id.leftmenu_list);
+        for (int i=0;i<viewGroup.getChildCount();i++){
+            View view = viewGroup.getChildAt(i);
+            if (view instanceof TableView){
+                view.setOnClickListener(this);
+            }
+        }
+
+        //初始化底部菜单
+        mContent = (LinearLayout) findViewById(R.id.act_home_content);
+        mBottomTabGroup = (BottomTabGroup) mContent.findViewById(R.id.tab_group);
+        mTab1 = (BottomTab) mBottomTabGroup.getChildAt(0);
+        mTab2 = (BottomTab) mBottomTabGroup.getChildAt(1);
+        mTab3 = (BottomTab) mBottomTabGroup.getChildAt(2);
+        mTab1.setChecked(true);  //设置为默认选中
+        //设置底部切换事件
+        mBottomTabGroup.setOnCheckedChangeListener(new BottomTabGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(BottomTabGroup root, int checkedId) {
+
+                switch (checkedId) {
+                    case R.id.tab_01:
+                        Toast.makeText(getBaseContext(), "第1个", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.tab_02:
+                        Toast.makeText(getBaseContext(), "第2个", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.tab_03:
+                        Toast.makeText(getBaseContext(), "第3个", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+
+        //以下是设置红点的代码
+        mTab1.getHintTextView().setBackgroundResource(R.drawable.hint_red);
+        mTab1.setHint("10");
+        mTab1.getHintTextView().setTextColor(0xffffffff);
+
     }
 
     /**
@@ -93,5 +141,38 @@ public class HomeActivity extends AppCompatActivity {
             mDrawerLayout.closeDrawer(mLeftMenu);
         else
             mDrawerLayout.openDrawer(mLeftMenu);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.leftmenu_myinfo:
+                Toast.makeText(HomeActivity.this,"个人信息",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.leftmenu_client:
+                Toast.makeText(HomeActivity.this,"客户信息",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.leftmenu_linkman:
+                Toast.makeText(HomeActivity.this,"联系人信息",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.leftmenu_project:
+                Toast.makeText(HomeActivity.this,"项目信息",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.leftmenu_check:
+                Toast.makeText(HomeActivity.this,"签到信息",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.leftmenu_feedback:
+                Toast.makeText(HomeActivity.this,"反馈信息",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.leftmenu_communication:
+                Toast.makeText(HomeActivity.this,"交流信息",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.leftmenu_notice:
+                Toast.makeText(HomeActivity.this,"交流信息",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.leftmenu_setting:
+                Toast.makeText(HomeActivity.this,"系统设置",Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
